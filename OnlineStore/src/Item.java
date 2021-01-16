371089f143ba20f7f64b02aafbfc5e7721a4fb23
DenysVodzyk <vodzyk@gmail.com>
2021-01-15 20:29:30 -0500

    Created item class

---
+++ OnlineStore/src/Item.java
@@ -0,0 +1,67 @@
+import java.time.LocalDate;
+
+public class Item {
+    private int id;
+    private String title;
+    private int code;
+    private String producer;
+    private LocalDate dateOfLastUpdate;
+
+    public Item(int id, String title, int code, String producer) {
+        this.id = id;
+        this.title = title;
+        this.code = code;
+        this.producer = producer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public LocalDate getDateOfLastUpdate() {
+        return dateOfLastUpdate;
+    }
+
+    public void setDateOfLastUpdate(LocalDate dateOfLastUpdate) {
+        this.dateOfLastUpdate = dateOfLastUpdate;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", code=" + code +
+                ", producer='" + producer + '\'' +
+                ", dateOfLastUpdate=" + dateOfLastUpdate +
+                '}';
+    }
+}
