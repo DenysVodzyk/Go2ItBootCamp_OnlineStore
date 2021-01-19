47fb6fc6433c1beb8983f930b663bf623552c95d
DenysVodzyk <vodzyk@gmail.com>
2021-01-18 22:23:06 -0500

    Code test

--- OnlineStore/src/Main.java
+++ OnlineStore/src/Main.java
@@ -4,6 +4,8 @@ import Service.CollectionService;
 import Service.ReportService;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,35 +15,42 @@ public class Main {
         Customer billC = new Customer("Bill Connor", LocalDate.of(1963, 7, 7), "353 Lilac Road Malden, MA 02148", Gender.MALE, "(554) 478-8654", new int[]{5, 6, 10}, LocalDate.of(2017, 6, 8));
         Customer shauntaM = new Customer("Shaunta Mcgeorge", LocalDate.of(1966, 9, 10), "7710 Glenwood Lane Centreville, VA 20120", Gender.FEMALE, new int[]{12, 14}, LocalDate.of(2017, 6, 10));
 
-        System.out.println(charaP.getLastPurchases());
-
-        CollectionService service = new CollectionService();
-
-        service.addCustomer(charaP);
-        service.addCustomer(lauraleeS);
-        service.addCustomer(billC);
-        service.addCustomer(shauntaM);
-
-        System.out.println(service.getWomenCustomers(service.getCustomers()));
-        System.out.println(service.getAllLastPurchases(service.getCustomers()));
-
-        System.out.println("ITEMS: ");
-
-        service.addItem(charaP);
-        service.addItem(lauraleeS);
-        service.addItem(billC);
-        service.addItem(shauntaM);
-
-        System.out.println(service.getItems());
-
-        System.out.println("Weekend");
-
-        System.out.println(service.getItemsDuringParticularWeekend(LocalDate.of(2017, 06, 02)));
-
+        CollectionService collectionService = new CollectionService();
         ReportService reportService = new ReportService();
 
-        System.out.println("------------------------");
-        reportService.mostPopularItems(service.getAllLastPurchases(service.getCustomers()));
+        collectionService.addCustomer(charaP);
+        collectionService.addCustomer(lauraleeS);
+        collectionService.addCustomer(billC);
+        collectionService.addCustomer(shauntaM);
+
+        collectionService.addItem(charaP);
+        collectionService.addItem(lauraleeS);
+        collectionService.addItem(billC);
+        collectionService.addItem(shauntaM);
+
+        System.out.println("All customers: ");
+        Set<Customer> allCustomers = collectionService.getCustomers();
+        System.out.println(allCustomers);
+
+        System.out.println("All women customers: ");
+        Set<Customer> women = collectionService.getWomenCustomers(allCustomers);
+        System.out.println(women);
+
+        System.out.println("All last purchases made by women: ");
+        List<Integer> womenPurchases = collectionService.getAllLastPurchases(women);
+        System.out.println(womenPurchases);
+
+        System.out.println("Goods most popular among women: ");
+        List<Integer> womenPopularPurchases = reportService.mostPopularItems(womenPurchases);
+        System.out.println(womenPopularPurchases);
+
+        System.out.println("Goods most popular during particular weekend: ");
+        List<Integer> allItems = collectionService.getAllLastPurchases(allCustomers);
+        List<Integer> itemsDuringParticularWeekend = collectionService.getItemsDuringParticularWeekend(LocalDate.of(2017, 6, 1));
+        List<Integer> popularItemsDuringWeekend = reportService.mostPopularItems(itemsDuringParticularWeekend);
+        System.out.println(popularItemsDuringWeekend);
+
+
 
 
 
