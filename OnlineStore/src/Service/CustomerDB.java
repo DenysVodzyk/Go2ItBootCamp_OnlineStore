deff9a6c7878d3f929e3241cd660b13e5ab00bc5
DenysVodzyk <vodzyk@gmail.com>
2021-01-21 21:59:21 -0500

    Minor changes

--- OnlineStore/src/Service/CustomerDB.java
+++ OnlineStore/src/Service/CustomerDB.java
@@ -9,20 +9,18 @@ import java.nio.file.Paths;
 
 public class CustomerDB {
     public static void main(String[] args) {
-        readCustomerFromFile();
+        CustomerDB db = new CustomerDB();
+        db.readCustomerFromFile();
     }
 
-    public static void readCustomerFromFile() {
+    public void readCustomerFromFile() {
         String path = "C:\\Users\\vodzy\\IdeaProjects\\Go2ItBootCamp_OnlineStore\\Customers.csv";
         try (BufferedReader reader = Files.newBufferedReader(Paths.get(path))) {
-           // reader.readLine();
             String input;
             while ((input = reader.readLine()) != null) {
-                String[] customerInfo = input.split(";");
-                System.out.println(customerInfo[1]);
-                System.out.println(customerInfo[0] + "---" + customerInfo [1] + "---" + customerInfo[2] + "---" + customerInfo[3] + "---" + customerInfo[4] + "---" + customerInfo[5] + customerInfo[6] );
+                  String[] customerInfo = input.split(";");
+                System.out.println(customerInfo[0]);
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
