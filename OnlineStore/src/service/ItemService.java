f7d47ec60bae016cbe2cc90ef88235bb5280d1c0
DenysVodzyk <vodzyk@gmail.com>
2021-02-26 22:10:40 -0500

    Service class with methods for Item entity

--- OnlineStore/src/service/ItemService.java
+++ OnlineStore/src/service/ItemService.java
@@ -1,19 +1,30 @@
-package Service;
+package service;
 
-import Entity.Customer;
-import Entity.Item;
-import Entity.Order;
+import entity.Customer;
+import entity.Item;
+import entity.Order;
+import repository.ItemRepository;
+import utils.FileReader;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ItemService {
-    private ReadFileService fileService = new ReadFileService();
+    private final DateTimeFormatter LAST_UPDATE = DateTimeFormatter.ofPattern("dd.MM.yyyy H:mm:ss");
+    private ItemRepository itemRepository;
 
-    public List<Item> getItems(String filePath) {
-        List<String> fileData = fileService.readFromFile(filePath);
+    public ItemService() {
+        init();
+    }
+
+    private void init() {
+        this.itemRepository = new ItemRepository();
+    }
+
+    public List<Item> parseAll(String filePath) {
+        List<String> fileData = FileReader.readFromFile(filePath);
         List<Item> items = new ArrayList<>();
 
         for (String item : fileData) {
@@ -22,39 +33,45 @@ public class ItemService {
             String title = itemData[1];
             int code = Integer.parseInt(itemData[2]);
             String producer = itemData[3];
-            LocalDate lastUpdate = getDateOfLastUpdate(itemData[4]);
-
+            LocalDateTime lastUpdate = LocalDateTime.parse(itemData[4], LAST_UPDATE);
             items.add(new Item(id, title, code, producer, lastUpdate));
         }
         return items;
     }
 
-    public Item getItemById(int id, String filePath) {
-        for (Item item : getItems(filePath)) {
-            if (item.getId() == id) {
-                return item;
+    public void addAllToDB(List<Item> items) {
+        for (Item item : items) {
+            if (getAll() == null || !getAll().contains(item)) {
+                itemRepository.addItem(item);
             }
         }
-        return null;
     }
 
-    public LocalDate getDateOfLastUpdate(String date) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy H:mm:ss");
-        return LocalDate.parse(date, formatter);
+    public List<Item> getAll() {
+        return itemRepository.getAll();
+    }
+
+    public Item getById(int id) {
+        return itemRepository.getById(id);
     }
 
     public List<Item> getItemsFromCustomers(List<Customer> customers) {
         List<Item> items = new ArrayList<>();
         for (Customer customer : customers) {
             for (Order order : customer.getOrder()) {
-                for (Item item : order.getItems()) {
-                    items.add(item);
-                }
+                items.addAll(order.getItems());
             }
         }
         return items;
     }
-
-
 }
 
+
+//    public Item getItemById(int id, String filePath) {
+//        for (Item item : getAll(filePath)) {
+//            if (item.getId() == id) {
+//                return item;
+//            }
+//        }
+//        return null;
+//    }
