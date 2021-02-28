1fcd72f7f64acc9d7978fc4d0366c43e8f0838ea
DenysVodzyk <vodzyk@gmail.com>
2021-02-27 23:15:51 -0500

    added hashCode and equals methods

--- OnlineStore/src/entity/Order.java
+++ OnlineStore/src/entity/Order.java
@@ -1,30 +1,39 @@
-package Entity;
+package entity;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Order {
-    private LocalDate oderDate;
+    private int id;
+    private LocalDate orderDate;
     private Customer customer;
-    private List<Item> items = new ArrayList<>();
+    private List<Item> items;
 
-    public Order(LocalDate oderDate, Customer customer, List<Item> items) {
-        this.oderDate = oderDate;
+    public Order(LocalDate orderDate, Customer customer, List<Item> items) {
+        this.orderDate = orderDate;
         this.customer = customer;
         this.items = items;
     }
 
-    public LocalDate getOderDate() {
-        return oderDate;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
     }
 
     public void addItem(Item item) {
         items.add(item);
     }
 
-    public void setOderDate(LocalDate oderDate) {
-        this.oderDate = oderDate;
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
     }
 
     public Customer getCustomer() {
@@ -35,6 +44,10 @@ public class Order {
         this.customer = customer;
     }
 
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
     public List<Item> getItems() {
         return items;
     }
@@ -42,9 +55,24 @@ public class Order {
     @Override
     public String toString() {
         return "Order{" +
-                "oderDate=" + oderDate +
+                "oderDate=" + orderDate +
                 ", customer=" + customer.getName() +
                 ", items=" + items +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(orderDate, order.orderDate) &&
+                Objects.equals(customer, order.customer) &&
+                Objects.equals(items, order.items);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderDate, customer, items);
     }
 }
