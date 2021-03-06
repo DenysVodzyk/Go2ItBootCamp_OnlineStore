996def2fdfd7c6323e9649905bb939f7bdca658d
DenysVodzyk <vodzyk@gmail.com>
2021-03-05 20:08:47 -0500

    moved method to Gender

--- OnlineStore/src/entity/Gender.java
+++ OnlineStore/src/entity/Gender.java
@@ -2,4 +2,11 @@ package entity;
 
 public enum Gender {
     MALE, FEMALE;
+
+    public static Gender getGender(String gender) {
+        if (gender.toLowerCase().equals("female")) {
+            return FEMALE;
+        }
+        return MALE;
+    }
 }
