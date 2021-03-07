eb61658904295b90b78ad5b9cafb50e03985fa87
DenysVodzyk <vodzyk@gmail.com>
2021-03-06 20:55:31 -0500

    class with constants

---
+++ OnlineStore/src/utils/Constant.java
@@ -0,0 +1,14 @@
+package utils;
+
+import java.time.format.DateTimeFormatter;
+
+public class Constant {
+    public static final DateTimeFormatter DOB_FORMAT = DateTimeFormatter.ofPattern("d MMMM yyyy");
+    public static final DateTimeFormatter LAST_PURCHASE_DATE_FORMAT = DateTimeFormatter.ofPattern("M/d/yyyy");
+    public static final DateTimeFormatter LAST_UPDATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy H:mm:ss");
+
+    public static final String ITEM_FILE_HEADER = "id;title;code;producer;dateOfLastUpdate";
+
+    public static final String ITEM_FILE_PATH = "OnlineStore/src/inputFiles/Items.csv";
+    public static final String CUSTOMER_FILE_PATH = "OnlineStore/src/inputFiles/Customers.csv";
+}
