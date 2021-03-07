cb599ff0097a8fc2715249cd5f98d13bb567f863
DenysVodzyk <vodzyk@gmail.com>
2021-03-07 17:50:08 -0500

    minor changes

--- OnlineStore/src/Execute.java
+++ OnlineStore/src/Execute.java
@@ -36,9 +36,9 @@ public class Execute {
         List<Order> ordersJava = fileReaderService.parseAllOrders(Constant.CUSTOMER_FILE_PATH);
 
         //add all items/customers/orders to DB
-        itemService.save(itemsJava);
-        customerService.save(customersJava);
-        orderService.save(ordersJava);
+//        itemService.save(itemsJava);
+//        customerService.save(customersJava);
+//        orderService.save(ordersJava);
 
         //Data from db
         List<Customer> customersSQL = customerService.getAll();
@@ -70,68 +70,3 @@ public class Execute {
 
     }
 }
-
-
-/*
-*
-        //items from Java
-        itemsJava.forEach(System.out::println);
-        System.out.println();
-
-        //add all items to DB
-        itemService.addAllToDB(itemsJava);
-        //show all items from DB
-        itemsSQL.forEach(System.out::println);
-        System.out.println();
-
-        //customers from Java
-        customersJava.forEach(System.out::println);
-        System.out.println();
-
-        //add all customers to DB
-        customerService.addAllToDB(customersJava);
-
-        //show all customers to DB
-        customersSQL.forEach(System.out::println);
-        System.out.println();
-
-        //show customer by ID and name
-        System.out.println(customerService.getById(1));
-        System.out.println(customerService.getByName("Katy Hoy"));
-        System.out.println();
-
-        //orders from Java
-        for (Order order : ordersJava) {
-            System.out.println(order.getId() + " " + order);
-        }
-        System.out.println();
-
-        //add all orders to DB
-        orderService.addAllToDB(ordersJava);
-
-        //show order by id
-        System.out.println(orderService.getById(1));
-        System.out.println();
-
-        //show all orders
-        ordersSQL.forEach(System.out::println);
-        System.out.println();
-
-        //show orders based on item id
-        orderService.getByItemId(2).forEach(System.out::println);
-        System.out.println();
-
-        //show order based on customer id
-        System.out.println(orderService.getByCustomerId(1));
-        System.out.println();
-
-        //Show female
-
-        for (Customer customer : customerService.getCustomersByGender(customersSQL, Gender.FEMALE)) {
-            for (Item item : customer.getItems()) {
-                System.out.print(" " + item.getId());
-            }
-        }
-
-        System.out.println();
-* */
