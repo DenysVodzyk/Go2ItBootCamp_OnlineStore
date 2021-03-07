05a826787b2b1c43668d92168ddb3b993d507baa
DenysVodzyk <vodzyk@gmail.com>
2021-03-06 20:41:07 -0500

    moved parser method to dedicated service class

--- OnlineStore/src/service/ItemService.java
+++ OnlineStore/src/service/ItemService.java
@@ -2,15 +2,11 @@ package service;
 
 import entity.Item;
 import repository.ItemRepository;
-import utils.FileReader;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ItemService {
-    private final DateTimeFormatter LAST_UPDATE = DateTimeFormatter.ofPattern("dd.MM.yyyy H:mm:ss");
     private ItemRepository itemRepository;
 
     public ItemService() {
@@ -21,23 +17,7 @@ public class ItemService {
         this.itemRepository = new ItemRepository();
     }
 
-    public List<Item> parseAll(String filePath) {
-        List<String> fileData = FileReader.readFromFile(filePath);
-        List<Item> items = new ArrayList<>();
-
-        for (String item : fileData) {
-            String[] itemData = item.split(";");
-            int id = Integer.parseInt(itemData[0]);
-            String title = itemData[1];
-            int code = Integer.parseInt(itemData[2]);
-            String producer = itemData[3];
-            LocalDateTime lastUpdate = LocalDateTime.parse(itemData[4], LAST_UPDATE);
-            items.add(new Item(id, title, code, producer, lastUpdate));
-        }
-        return items;
-    }
-
-    public void addAllToDB(List<Item> items) {
+    public void save(List<Item> items) {
         for (Item item : items) {
             itemRepository.add(item);
         }
